0e4ab2d78edde65a0be8d241bdbe96f90fbfb338
marco-soria <marco.soriaa7@gmail.com>
2025-06-10 10:35:10 -0500

    handling errors

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,10 +1,11 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,6 +17,7 @@ import { HttpClient } from '@angular/common/http';
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal<boolean>(false);
+  error = signal<string | null>('');
   // Using inject to get the HttpClient and DestroyRef instances
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
@@ -26,10 +28,24 @@ export class AvailablePlacesComponent implements OnInit {
     // Using the HttpClient to fetch places from the backend
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please try again later.'
+              )
+          );
+        })
+      )
       .subscribe({
         next: (places) => {
           this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
         complete: () => {
           this.isFetching.set(false);
