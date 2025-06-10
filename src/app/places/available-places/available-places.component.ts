ba78097e59808bef5f3aa228c30dba885b5cd02f
marco-soria <marco.soriaa7@gmail.com>
2025-06-10 09:08:23 -0500

    implementing a get method

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,30 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  //constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((resData) => resData.places))
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+      });
+    // .subscribe({
+    //   next: (resData) => {
+    //     //console.log(resData);
+    //     this.places.set(resData.places);
+    //   },
+    // });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
