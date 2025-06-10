f42e1d735f1ed11334c21d8f56f654d87a9e9377
marco-soria <marco.soriaa7@gmail.com>
2025-06-10 09:17:29 -0500

    fallback text for get requests

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -15,17 +15,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal<boolean>(false);
+  // Using inject to get the HttpClient and DestroyRef instances
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   //constructor(private httpClient: HttpClient) {}
   ngOnInit(): void {
+    this.isFetching.set(true);
+    // Using the HttpClient to fetch places from the backend
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(map((resData) => resData.places))
       .subscribe({
         next: (places) => {
           this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
         },
       });
     // .subscribe({
